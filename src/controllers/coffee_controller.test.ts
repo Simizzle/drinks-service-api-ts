c8f3a7c6926a9ee2850d0b84fda188e744c5250d
Simon Morgan <simonmorgan@Admins-MacBook-Pro-3.local>
2023-05-26 09:51:15 +0100

    feat: extra coffee controller tests created for queryParams and no queryParam

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -10,3 +10,25 @@ test("GET /coffee should return correct object", async () => {
     name: "Latte",
   });
 });
+
+test("GET /coffee with queryParam should return correct object", async () => {
+  const res = await request(app)
+    .get("/coffee")
+    .query({ coffeeName: "Americano" });
+
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Americano",
+  });
+});
+
+test("GET /coffee with no queryParam should return correct object", async () => {
+  const res = await request(app).get("/coffee");
+
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Latte",
+  });
+});
